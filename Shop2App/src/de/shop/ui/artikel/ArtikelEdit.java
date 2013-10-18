1d1cab21d3e2fb777f8e41fc3999847d2995cfbf
chuzzla <e_realize@yahoo.de>
2013-10-18 17:12:13 +0200

    ggg

--- Shop2App/src/de/shop/ui/artikel/ArtikelEdit.java
+++
@@ -1,7 +0,0 @@
-package de.shop.ui.artikel;
-
-import android.app.Fragment;
-
-public class ArtikelEdit extends Fragment {
-
-}
--- shop/src/main/java/de/shop/kundenverwaltung/domain/GeschlechtType.java
+++
@@ -1,6 +0,0 @@
-package de.shop.kundenverwaltung.domain;
-
-public enum GeschlechtType {
-	MAENNLICH,
-	WEIBLICH;
-}
